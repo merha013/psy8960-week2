7c6a741f1197c94fa0b0c7f64b994f22a6017076
Toni Merhar <merha013@umn.edu>
2024-01-29 18:43:29 -0600

    Corrections and additional comments from class

--- R/week2.R
+++ R/week2.R
@@ -5,7 +5,15 @@ rt_df$gender <- factor(rt_df$gender, levels=c("M","F","N"), labels=c("Male","Fem
 
 # Analysis
 mean(rt_df$rt)
-rt_f_df <- data.frame(rt_df, rt_df$gender=="Female")
+rt_f_df <- subset(rt_df, rt_df$gender=="Female")
 hist(rt_f_df$rt, main="Histogram of Female Reaction Times", xlab="Reaction Times")
 datasets <- list(rt_df, rt_f_df)
 datasets[[1]][2]
+# above says [[extract]] [subset]
+
+# Another option for how to subset Female gender
+rt_f_df2 <- rt_df[rt_df$gender == "Female",]
+
+# Another option for how to display the contents of the rt variable from the first dataset contained within datasets
+datasets[[1]][[2]]
+datasets[[1]]$rt
